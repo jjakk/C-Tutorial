a7fd436a8d56bb666f32b35325636b19adc9aec3
John Khachian <jaksawesome10@gmail.com>
2022-06-14 13:42:10 -0400

    functions and return statements

---
+++ functions/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sayHi(char name[], int age){
+    printf("Hello %s you are %i\n", name, age);
+}
+
+int main(){
+
+    sayHi("Mike", 40);
+    sayHi("Tom", 23);
+    sayHi("Oscar", 70);
+    return 0;
+}
