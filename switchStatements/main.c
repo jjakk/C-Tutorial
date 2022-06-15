6a2f7e88fd2895190e5deae8a1bd484e010ac5e6
John Khachian <jaksawesome10@gmail.com>
2022-06-15 11:41:49 -0400

    better calculator, and switch statements

---
+++ switchStatements/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+
+    char grade = 'C';
+
+    switch(grade){
+        case 'A':
+            printf("You did great!");
+            break;
+        case 'B':
+            printf("You did alright!");
+            break;
+        case 'C':
+            printf("You did poorly");
+            break;
+        case 'D':
+            printf("You did very bad");
+            break;
+        case 'F':
+            printf("You Failed!");
+            break;
+        default:
+            printf("Invalid Grade");
+    }
+
+    return 0;
+}
