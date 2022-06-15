7b1b51eff4efef59963172579034fdf8789a1ee7
John Khachian <jaksawesome10@gmail.com>
2022-06-15 12:00:23 -0400

    structs and while loops

---
+++ whileLoops/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+
+    int index = 1;
+    do {
+        printf("%d\n", index);
+        index++;
+    } while(index <= 5);
+
+    return 0;
+}
